e7848e06c183a134c91490dc0bf535ed0f02f06d
Sergey Tolstinskiy <tolstinskysv@ekassir.com>
2020-01-10 22:27:07 +0300

    Add reddit VM create script

---
+++ config-scripts/create-reddit-vm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#create reddit VM from full image
+gcloud compute instances create reddit-app \
+  --boot-disk-size=10GB \
+  --image-family=reddit-full \
+  --machine-type=f1-micro \
+  --tags=puma-server \
+  --restart-on-failure
